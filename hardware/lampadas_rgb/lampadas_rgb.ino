ebb37fbe0824e24aaf5bbf6a096c2e01926212c5
csilabinatel <csi-adm@inatel.br>
2026-05-21 15:37:55 -0300

    Update lampadas_rgb.ino

--- hardware/lampadas_rgb/lampadas_rgb.ino
+++ hardware/lampadas_rgb/lampadas_rgb.ino
@@ -1,5 +1,5 @@
 //DECLARACAO DAS BIBLIOTECAS
-#include <ESP8266WiFi.h>      // Wi-Fi
+#include <WiFi.h>         //Wi-Fi
 #include <PubSubClient.h>     // MQTT
 #include <time.h>             // UTC
 #include <strings.h>          // String manipulation
@@ -7,13 +7,15 @@
 #include <ArduinoJson.h>
 
 const int keys[] = {3, 2, 1, 6, 5, 4, 9, 8, 7, 0};
+// ligar -1 acessar a posição do vetor que estara correto 
+//Mapeação da ordem das lampadas // troca de 3 por 1,  nuemro da lampada pelo vetor 
 
 //informações da rede WIFI
 const char* ssid = "CSI-Lab"; //SSID da rede WIFI
 const char* password =  "In@teLCS&I"; //senha da rede wifi
 
 //informações do broker MQTT
-const char* mqttServer = "192.168.40.7";   //servidor
+const char* mqttServer = "192.168.66.11";   //servidor
 const char* mqttUser = "csilab";              //usuário
 const char* mqttPassword = "WhoAmI#2024";      //senha
 const int mqttPort = 1883;                     //porta
@@ -25,73 +27,137 @@ PubSubClient client(espClient);
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 
-#define PIN D2
+#define PIN 5
 #define NUMPIXELS 63
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 #define DELAYVAL 50 // Time (in milliseconds) to pause between pixels
 
 //FUNCAO DE RECEBIMENTO E PROCESSAMENTO DE MENSAGENS VINDAS DO BROKER
 void callback(char* topic, byte* payload, unsigned int length){
-  //armazena mensagem recebida em uma variavel inteira
-  payload[length] = '\0';
-  int MSG = atoi((char*)payload);
+
+  // Cria buffer seguro para a mensagem
+  char msg[length + 1];
+
+  memcpy(msg, payload, length);
+
+  msg[length] = '\0';
+
   String topico = topic;
-  
+
   Serial.println();
   Serial.println("-----------------------");
 
-  //MOSTRANDO RECEBIMENTO DA MENSAGEM NA SERIAL
   Serial.print("Mensagem no tópico: ");
   Serial.println(topico);
-  Serial.print("Mensagem:");
-  Serial.print(MSG);
 
-  Serial.println();
+  Serial.print("Mensagem recebida: ");
+  Serial.println(msg);
+
   Serial.println("-----------------------");
   Serial.println();
 
   int rgb[3];
 
-  //{"lampada":1,"r":0,"g":0,"b":0} exemplo de mensagem recebida
-  if (topico == "rgb_module/dimmer/setLampState"){
-    const char* msg = (char*)payload; // Define o tamanho do buffer necessário para armazenar o JSON
-    StaticJsonDocument<200> doc; // Deserializa a string JSON para o objeto doc
-    DeserializationError error = deserializeJson(doc, msg); // Verifica se houve erro na deserialização
+  // =========================
+  // TOPICO VEIA
+  // =========================
+  if(topico == "rgb_module/veia/setLampState"){
+
+    StaticJsonDocument<200> doc;
+
+    DeserializationError error = deserializeJson(doc, msg);
+
     if (error) {
-      Serial.print(F("Falha na deserialização: "));
-      Serial.println(error.f_str());
+
+      Serial.print("Erro JSON: ");
+
+      Serial.println(error.c_str());
+
+      return;
+    }
+
+    int lamp = doc["lampada"];
+
+    // Proteção
+    if(lamp < 1 || lamp > 9){
+
+      Serial.println("Lampada invalida");
+
+      return;
+    }
+
+    rgb[0] = doc["r"];
+    rgb[1] = doc["g"];
+    rgb[2] = doc["b"];
+
+    Serial.print("Lampada: ");
+    Serial.println(lamp);
+
+    Serial.print("R: ");
+    Serial.println(rgb[0]);
+
+    Serial.print("G: ");
+    Serial.println(rgb[1]);
+
+    Serial.print("B: ");
+    Serial.println(rgb[2]);
+
+    lampada(keys[lamp - 1], rgb);
+  }
+
+  // =========================
+  // TOPICO DIMMER
+  // =========================
+  if (topico == "rgb_module/dimmer/setLampState"){
+
+    StaticJsonDocument<200> doc;
+
+    DeserializationError error = deserializeJson(doc, msg);
+
+    if (error) {
+
+      Serial.print("Erro JSON: ");
+
+      Serial.println(error.c_str());
+
       return;
     }
 
     if (doc["dimmer"].isNull()){
-      if (doc["right_hand_message"] == 0) { rgb[0] = 0; rgb[1] = 0; rgb[2] = 0; }
-      if (doc["right_hand_message"] == 1) { rgb[0] = 255; rgb[1] = 255; rgb[2] = 255;}
+
+      if (doc["right_hand_message"] == 0){
+
+        rgb[0] = 0;
+        rgb[1] = 0;
+        rgb[2] = 0;
+      }
+
+      if (doc["right_hand_message"] == 1){
+
+        rgb[0] = 255;
+        rgb[1] = 255;
+        rgb[2] = 255;
+      }
+
       int lampNum = doc["left_hand"];
-      lampada(keys[lampNum-1],rgb);
+
+      lampada(keys[lampNum - 1], rgb);
     }
+
     if (doc["right_hand_message"].isNull()){
-      rgb[0] = doc["dimmer"]; rgb[1] = doc["dimmer"]; rgb[2] = doc["dimmer"];
+
+      rgb[0] = doc["dimmer"];
+      rgb[1] = doc["dimmer"];
+      rgb[2] = doc["dimmer"];
+
       int lampNum = doc["left_hand"];
-      lampada(keys[lampNum-1],rgb);
+
+      lampada(keys[lampNum - 1], rgb);
     }
-  }
-  if(topico == "rgb_module/veia/setLampState"){
-    const char* msg = (char*)payload; // Define o tamanho do buffer necessário para armazenar o JSON
-    StaticJsonDocument<200> doc; // Deserializa a string JSON para o objeto doc
-    DeserializationError error = deserializeJson(doc, msg); // Verifica se houve erro na deserialização
-    if (error) {
-      Serial.print(F("Falha na deserialização: "));
-      Serial.println(error.f_str());
-      return;
-    }
-    rgb[0] = doc["r"];
-    rgb[1] = doc["g"];
-    rgb[2] = doc["b"];
-    lampada(doc["lampada"],rgb);
   }
 }
   
@@ -117,7 +183,7 @@ void connect () //FUNCAO DE RECONEXAO COM O BROKER
 
 void setup()
 {
-  pinMode(D2,OUTPUT);
+  pinMode(5,OUTPUT);
   Serial.begin(115200); //Velocidade da Serial
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
